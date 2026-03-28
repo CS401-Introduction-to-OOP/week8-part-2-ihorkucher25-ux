26fa6faa2e6dfb1610bf45e2c8780aef435382a1
Ihor Kucher <IhorKucher25@kse.org.ua>
2026-03-28 17:19:20 +0200

    Stage 2

---
+++ Week8/Task2/Task3/DataContainer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+namespace Task3;
+
+public class DataContainer<T> where T : DataItem
+{
+    private readonly List<T> items = new();
+
+    public void AddItem(T item)
+    {
+        items.Add(item);
+    }
+
+    public long GetTotalSize()
+    {
+        return items.Count();
+    }
+}
